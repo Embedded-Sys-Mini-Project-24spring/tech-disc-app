f7245b930d9883d515640f8b69f883185024b331
kosumi <chankocyo@gmail.com>
2024-03-31 12:14:15 -0600

    Fix type check

--- src/components/SerialControl.vue
+++ src/components/SerialControl.vue
@@ -15,7 +15,7 @@ import {
   Title,
   Tooltip,
   Legend,
-  ChartData
+  type ChartData
 } from 'chart.js'
 
 ChartJS.register(
@@ -30,12 +30,13 @@ ChartJS.register(
 
 const serial = new SerialMonitor({mode: "text", parseLines: true});
 
-const accel_x = ref([])
-const accel_y = ref([])
-const accel_z = ref([])
-const cnt = ref([])
+const accel_x = ref<Array<Number>>([])
+const accel_y = ref<Array<Number>>([])
+const accel_z = ref<Array<Number>>([])
+const cnt = ref<Array<Number>>([])
 
-const messages = ref("")
+const messages = ref<Array<String>>([])
+const messages_str = computed(() => {return messages.value.toString()}) 
 const re_acc = /accel_xout:(-?[0-9]+).;accel_yout:(-?[0-9]+).;accel_zout:(-?[0-9]+);/;
 
 function update_chartData() {
@@ -80,22 +81,32 @@ const handleSerialEvent = (ev) => {
     if (re_acc.test(ev.detail)) {
         // console.log(ev.detail);
         const match = ev.detail.match(re_acc);
+
         accel_x.value.push(Number(match[1]))
         accel_y.value.push(Number(match[2]))
         accel_z.value.push(Number(match[3]))
+        cnt.value.push(cnt.value.length)
+        messages.value.push(ev.detail);
+        const MAX_LEN = 1000;
+        const diff = cnt.value.length - MAX_LEN;
+        if (diff > 0) {
+            accel_x.value.splice(0, diff)
+            accel_y.value.splice(0, diff)
+            accel_z.value.splice(0, diff)
+            messages.value.splice(0, diff)
+            cnt.value.splice(0, diff)
+        }
 
         const span = 100
-        cnt.value.push(cnt.value.length)
         chartData.value = setChartData(
-            [...cnt.value.slice(-span)], 
-            [...accel_x.value.slice(-span)],
-            [...accel_y.value.slice(-span)],
-            [...accel_z.value.slice(-span)]
+            cnt.value.slice(-span), 
+            accel_x.value.slice(-span),
+            accel_y.value.slice(-span),
+            accel_z.value.slice(-span)
         )
         //console.log(chartData.value)
         //console.log("match: " + match[1].toString() + " " + match[2].toString())
     }
-    messages.value += ev.detail;
 
 }
 
@@ -183,7 +194,7 @@ function open_close() {
         <Dropdown v-model="baud_rate" :options="baud_rates" placeholder="Select a baud rate" class="w-full md:w-14rem" />
         <Button v-bind:label="port_open_str" @click="open_close"/>
     </div>
-    <Textarea v-model="messages" rows="20" cols="90" disabled/>
+    <Textarea v-model="messages_str" rows="20" cols="90" disabled/>
     <div class="card">
         <Line :data="chartData" :options="chartOptions" />
         <!-- <Chart type="line" :data="chartData" :options="chartOptions" class="h-30rem" /> -->
