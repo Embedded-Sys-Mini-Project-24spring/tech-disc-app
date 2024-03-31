d7ff3b3b92d78fafe48be4ce651d7000532349d8
kosumi <chankocyo@gmail.com>
2024-03-31 02:03:30 -0600

    Add textarea

--- src/components/SerialControl.vue
+++ src/components/SerialControl.vue
@@ -2,10 +2,15 @@
 import { ref, computed } from 'vue' 
 import Dropdown from 'primevue/dropdown';
 import Button from 'primevue/button';
+import Textarea from 'primevue/textarea';
 import SerialMonitor from '@ridge18/web-serial-monitor';
 
 const serial = new SerialMonitor({mode: "text", parseLines: true});
+
+const messages = ref("")
+
 const handleSerialEvent = (ev) => {
+    messages.value += ev.detail;
     console.log(ev.detail);
 }
 
@@ -46,6 +51,8 @@ function open_close() {
         <Dropdown v-model="baud_rate" :options="baud_rates" placeholder="Select a baud rate" class="w-full md:w-14rem" />
         <Button v-bind:label="port_open_str" @click="open_close"/>
     </div>
+
+    <Textarea v-model="messages" rows="20" cols="90" disabled/>
 
 </template>
 
